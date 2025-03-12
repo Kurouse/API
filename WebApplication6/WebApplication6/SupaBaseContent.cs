4cf9aa06c340c66bfd9b71b615af420eec86f644
Kurouse <146446650+Kurouse@users.noreply.github.com>
2025-03-12 16:41:23 +0500

    Add files via upload

---
+++ WebApplication6/WebApplication6/SupaBaseContent.cs
@@ -0,0 +1,18 @@
+﻿using Supabase.Postgrest.Models;
+using Supabase.Postgrest.Attributes;
+
+namespace WebApplication6
+{
+    public class SupaBaseContent
+    {
+        public SupaBaseContent()
+        {
+        }
+
+       public async Task<List<User>> GetUsers(Supabase.Client _supabaseClient ) 
+        {
+            var result = await _supabaseClient.From<User>().Get();
+            return result.Models;
+        }
+    }
+}
